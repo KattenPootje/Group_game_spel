64b21412686505a66f47ff5956457fc7c0b2126f
KjeltV <kjeltverdronk@gmail.com>
2026-05-28 09:53:40 +0200

    scenes voor iedereen

--- Assets/Scripts/Arsenal.cs
+++ Assets/Scripts/Arsenal.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 public class Item
 {
     public string name;
-    public string type;
+    public string weaponType;
+    public string fireMode;
+    public float fireRate;
+    public float recoil;
     public int damage;
-    public Vector3 DefaultWeaponOffsetPosition;
-    public Quaternion DefaultWeaponOffsetRotation;
+    public Vector3 WeaponOffsetPosition;
+    public Quaternion WeaponOffsetRotation;
     public Vector3 SprintWeaponOffsetPosition;
     public Quaternion SprintWeaponOffsetRotation;
 }
@@ -16,8 +19,8 @@ public class Arsenal : MonoBehaviour
 {
     public Item[] Items =
     {
-        new Item { name = "mjolnir", type = "melee", damage = 55, DefaultWeaponOffsetPosition = new Vector3(0.5f, -0.5f, 1f), DefaultWeaponOffsetRotation = Quaternion.Euler(0f, 0f, 0f), SprintWeaponOffsetPosition = new Vector3(0.5f, -0.8f, 1f), SprintWeaponOffsetRotation = Quaternion.Euler(0f, 45f, 35f) },
-        new Item { name = "Weapon2", type = "gun", damage = 60, DefaultWeaponOffsetPosition = new Vector3(0.5f, -0.8f, 1f), DefaultWeaponOffsetRotation = Quaternion.Euler(0f, 0f, 0f), SprintWeaponOffsetPosition = new Vector3(0.5f, -0.8f, 1f), SprintWeaponOffsetRotation = Quaternion.Euler(0f, 45f, 35f) },
-        new Item { name = "Weapon3", type = "gun", damage = 70, DefaultWeaponOffsetPosition = new Vector3(0.5f, -0.8f, 1f), DefaultWeaponOffsetRotation = Quaternion.Euler(0f, 0f, 0f), SprintWeaponOffsetPosition = new Vector3(0.5f, -0.8f, 1f), SprintWeaponOffsetRotation = Quaternion.Euler(0f, 45f, 35f) }
+        new Item { name = "mjolnir", weaponType = "melee", fireMode = "", fireRate = 150, recoil = 0, damage = 55, WeaponOffsetPosition = new Vector3(0.5f, -0.5f, 1f), WeaponOffsetRotation = Quaternion.Euler(0f, 0f, 0f), SprintWeaponOffsetPosition = new Vector3(0.15f, -0.7f, 1f), SprintWeaponOffsetRotation = Quaternion.Euler(0f, 315f, 30f) },
+        new Item { name = "Weapon2", weaponType = "gun", fireMode = "semi", fireRate = 500, recoil = .6f, damage = 60, WeaponOffsetPosition = new Vector3(0.5f, -0.8f, 1f), WeaponOffsetRotation = Quaternion.Euler(0f, 0f, 0f), SprintWeaponOffsetPosition = new Vector3(0.15f, -0.7f, 1f), SprintWeaponOffsetRotation = Quaternion.Euler(0f, 315f, 30f) },
+        new Item { name = "Weapon3", weaponType = "gun", fireMode = "full", fireRate = 800, recoil = .3f, damage = 70, WeaponOffsetPosition = new Vector3(0.5f, -0.8f, 1f), WeaponOffsetRotation = Quaternion.Euler(0f, 0f, 0f), SprintWeaponOffsetPosition = new Vector3(0.15f, -0.7f, 1f), SprintWeaponOffsetRotation = Quaternion.Euler(0f, 315f, 30f) }
     };
 }
